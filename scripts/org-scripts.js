bfcc6e77122417d0394f49e3aabd81615a366f4e
Robe <2235422@slu.edu.ph>
2025-10-21 23:39:39 +0800

    btn update

--- scripts/org-scripts.js
+++ scripts/org-scripts.js
@@ -2,7 +2,23 @@ function activeBtn(btn) {
     btn.style.backgroundColor = "#FFD700";
     btn.style.color = "#003366";
 }
+function inactiveBtn(btn) {
+    btn.style.backgroundColor = "#003366";
+    btn.style.color = "white";
+}
 const dashBtn = document.getElementById("dash-btn");
+const submHistryBtn = document.getElementById("sub-hist-btn");
+
 activeBtn(dashBtn);
+
+//onclicks
+function showDashboard() {
+    activeBtn(dashBtn);
+    inactiveBtn(submHistryBtn);
+}
+function showHistory() {
+    activeBtn(submHistryBtn);
+    inactiveBtn(dashBtn);
+}
 
 
