0d10d90bbd16c9ea16e9a7b58840885c2bf05329
Robe <2235422@slu.edu.ph>
2025-10-21 23:31:46 +0800

    Manually-coded

---
+++ scripts/org-scripts.js
@@ -0,0 +1,8 @@
+function activeBtn(btn) {
+    btn.style.backgroundColor = "#FFD700";
+    btn.style.color = "#003366";
+}
+const dashBtn = document.getElementById("dash-btn");
+activeBtn(dashBtn);
+
+
